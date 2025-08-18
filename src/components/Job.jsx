ba21e5699ea7d65051b0638033559b950abb6844
hyzeuline <iseline.voison@gmail.com>
2025-08-18 18:47:17 +0200

    fix :css

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,11 +1,13 @@
 const Job = props => {
   return (
-    <article className={props.className}>
+    <div className={`item ${props.color}`}>
       <h2>{props.title}</h2>
-      <div>{props.contractType}</div>
-      <div>{props.country}</div>
-      <div>{props.city}</div>
-    </article>
+      <div>
+        <div>{props.contractType}</div>
+        <div>{props.country}</div>
+        <div>{props.city}</div>
+      </div>
+    </div>
   );
 };
 
--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -2,88 +2,70 @@ import Job from "./Job";
 
 const Jobs = () => {
   return (
-    <div class="container">
-      <div class="item">
-        <Job
-          className="sales"
-          title="Full Time Sales Associate"
-          contractType="CDI"
-          country="Australie"
-          city="Sydney"
-        />
-      </div>
-      <div class="item">
-        <Job
-          className="security"
-          title="Agent de sécurité"
-          contractType="CDI"
-          country="France"
-          city="Pantin"
-        />
-      </div>
-      <div class="item">
-        <Job
-          className="industry"
-          title="Responsable d'Atelier (H/F)"
-          contractType="CDD"
-          country="France"
-          city="Paris"
-        />
-      </div>
-      <div class="item">
-        <Job
-          className="health"
-          title="Cheffe de projets"
-          contractType="CDD"
-          country="France"
-          city="Paris"
-        />
-      </div>
-      <div class="item">
-        <Job
-          className="tech"
-          title="Développeur react.js"
-          contractType="CDI"
-          country="France"
-          city="Paris"
-        />
-      </div>
-      <div class="item">
-        <Job
-          className="sales"
-          title="Sales associate"
-          contractType="CDI"
-          country=" Stockholm"
-          city="Suède"
-        />
-      </div>
-      <div class="item">
-        <Job
-          className="sales"
-          title="Venderu-Crans montana"
-          contractType="CDI"
-          country="Suisse"
-          city="Crans-Montana"
-        />
-      </div>
-      <div class="item">
-        <Job
-          className="sales"
-          title="CRM & Data Quality Analyst"
-          contractType="CDI"
-          country="USA"
-          city="New York"
-        />
-      </div>
-      <div class="item">
-        <Job
-          className="health"
-          title="Infirmier"
-          contractType="CDI"
-          country="France"
-          city="Pantin"
-        />
-      </div>
+    <div className="container">
+      <Job
+        color="red"
+        title="Full Time Sales Associate"
+        contractType="CDI"
+        country="Australie"
+        city="Sydney"
+      />
+      <Job
+        color="green"
+        title="Agent de sécurité"
+        contractType="CDI"
+        country="France"
+        city="Pantin"
+      />
+      <Job
+        color="yellow"
+        title="Responsable d'Atelier (H/F)"
+        contractType="CDD"
+        country="France"
+        city="Paris"
+      />
+      <Job
+        color="blue"
+        title="Cheffe de projets"
+        contractType="CDD"
+        country="France"
+        city="Paris"
+      />
+      <Job
+        color="pink"
+        title="Développeur react.js"
+        contractType="CDI"
+        country="France"
+        city="Paris"
+      />
+      <Job
+        color="red"
+        title="Sales associate"
+        contractType="CDI"
+        country=" Stockholm"
+        city="Suède"
+      />
+      <Job
+        color="green"
+        title="Venderu-Crans montana"
+        contractType="CDI"
+        country="Suisse"
+        city="Crans-Montana"
+      />
+      <Job
+        color="yellow"
+        title="CRM & Data Quality Analyst"
+        contractType="CDI"
+        country="USA"
+        city="New York"
+      />
+      <Job
+        color="blue"
+        title="Infirmier"
+        contractType="CDI"
+        country="France"
+        city="Pantin"
+      />
     </div>
   );
 };
